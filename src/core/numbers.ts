83c91a8439be2081f489d9ae29d8a6a1c49ed921
Peter Haiduk <peter.haiduk@fute.dev>
2024-03-11 20:24:23 +0100

    randomNumber, roundToDecimals, formatFixedDecimals

--- src/core/numbers.ts
+++ src/core/numbers.ts
@@ -1,3 +1,18 @@
-export function formatDecimals(number: number, numberofDecimals: number) {
-  return Math.round(number * 10 ** numberofDecimals) / 10 ** numberofDecimals;
+/** */
+export function randomNumber(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+/** */
+export function roundToDecimals(number: number, numberOfDecimals: number) {
+  return Math.round(number * 10 ** numberOfDecimals) / 10 ** numberOfDecimals;
+}
+
+/** */
+export function formatFixedDecimals(number: number, numberOfDecimals: number) {
+  const wholeNumber = Math.round(number);
+  const decimals = Math.abs(
+    Math.round((number - wholeNumber) * 10 ** numberOfDecimals),
+  );
+  return `${wholeNumber}.${decimals}`;
 }
