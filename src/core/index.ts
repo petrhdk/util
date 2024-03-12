74db49119aa742a659386f40aea198c34ca3cbfe
Peter Haiduk <peter.haiduk@fute.dev>
2024-03-12 09:22:37 +0100

    isNotDefined, assertThatIsNeverCalled

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,14 @@
 export * from './numbers';
 
-/** Check value against `undefined` and `null`. */
+/** returns `true` if value is everything but `null` or `undefined` */
 export function isDefined<T>(value: T): value is Exclude<T, null | undefined> {
   return value !== undefined && value !== null;
 }
+
+/** returns `true` if value is `null` or `undefined` */
+export function isNotDefined(value: any): value is undefined | null {
+  return value === undefined || value === null;
+}
+
+/* utility function to assert exhaustiveness of switch statements */
+export function assertThatIsNeverCalled(_: never) {}
