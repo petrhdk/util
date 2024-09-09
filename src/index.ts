111446e4fdb5f44def8e7e4ed8e07ad412ab07d9
Peter Haiduk <peter.haiduk@fute.dev>
2024-09-09 22:14:11 +0200

    Feature: isDefined, isNotDefined, randomNumber, roundToDecimals, formatFixedDecimals, assertThatIsNeverCalled

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/**
+ * returns `true` if value is anything but `undefined` or `null`
+ */
+export function isDefined<T>(value: T): value is Exclude<T, null | undefined> {
+  return value !== undefined && value !== null;
+}
+
+/**
+ * returns `true` if value is `undefined` or `null`
+ */
+export function isNotDefined(value: any): value is undefined | null {
+  return value === undefined || value === null;
+}
+
+/**
+ * generate a random number in the given number interval
+ */
+export function randomNumber(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+/**
+ * round the given number to a maximum number of decimals
+ */
+export function roundToDecimals(number: number, numberOfDecimals: number) {
+  return Math.round(number * 10 ** numberOfDecimals) / 10 ** numberOfDecimals;
+}
+
+/**
+ * convert the number to string with fixed number of decimals (filled with zeros if necessary)
+ */
+export function formatFixedDecimals(number: number, numberOfDecimals: number) {
+  const wholeNumber = Math.round(number);
+  const decimals = Math.abs(
+    Math.round((number - wholeNumber) * 10 ** numberOfDecimals),
+  )
+    .toString()
+    .padEnd(numberOfDecimals, '0');
+  return `${wholeNumber}.${decimals}`;
+}
+
+/**
+ * utility function to assert exhaustiveness of `switch` or `if` statements
+ *
+ * example:
+ *  ```
+ *  switch(value) {
+ *    case 1: break;
+ *    case 2: break;
+ *    default: assertThatIsNeverCalled(value);
+ *  }
+ *  ```
+ */
+export function assertThatIsNeverCalled(_: never) {}
